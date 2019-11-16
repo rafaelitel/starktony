26f0e1f47ff0d91d09a6161d98e7d5ca4c2f8d0a
anthony stark <40017611+rafaelitel@users.noreply.github.com>
2019-11-16 22:22:07 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -211,7 +211,6 @@ cat /etc/wireguard/wg_${host}_2.conf   && next
 cat /etc/wireguard/wg_${host}_3.conf   && next
 
 echo_RedBG   " One-Step Automated Install WireGuard Script For Debian_9 Ubuntu Centos_7 "
-echo_GreenBG "      Open Source Project: https://github.com/hongwenjun/vps_setup        "
 
 echo_Yellow  ":: WireGuard Management Command."
 echo_SkyBlue  "Usage: ${GreenBG} bash wgmtu ${SkyBlue} [ setup | remove | vps | bench | -U ] "
